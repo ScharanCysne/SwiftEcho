f9d7f8e0e8f9cd73d2d5fc422e5753dbf6996a97
nicholas-scharan <nicholas_scharan@mckinsey.com>
2024-11-10 12:22:15 -0300

    Initial Commit

---
+++ echo/echoApp.swift
@@ -0,0 +1,17 @@
+//
+//  echoApp.swift
+//  echo
+//
+//  Created by Nicholas Scharan on 10/11/24.
+//
+
+import SwiftUI
+
+@main
+struct echoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
